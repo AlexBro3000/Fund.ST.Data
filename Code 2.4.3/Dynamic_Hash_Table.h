0fe97748cb66c9aa70f9f10aefe2132d9603803c
AlexBro3000 <61786578+AlexBro3000@users.noreply.github.com>
2022-06-25 23:21:48 +1000

    Update Dynamic_Hash_Table.h

--- Code 2.4.3/Dynamic_Hash_Table.h
+++ Code 2.4.3/Dynamic_Hash_Table.h
@@ -13,6 +13,16 @@ struct Ingredients
 	string units;
 	size_t calories;
 };
+inline bool operator==(const Ingredients& lhs, const Ingredients& rhs)
+{
+	if (lhs.name == rhs.name && lhs.units == rhs.units && lhs.calories == rhs.calories)
+		return true;
+	return false;
+}
+inline bool operator!=(const Ingredients& lhs, const Ingredients& rhs)
+{
+	return lhs.name != rhs.name;
+}
 
 struct Dynamic_Hash_Table_Cell
 {
@@ -30,18 +40,20 @@ public:
 	~Dynamic_Hash_Table();
 
 	void add(Ingredients* data);
-	void delete_key(string key);
-	bool seach(string key);
+	void delete_key(Ingredients* data);
+	bool seach(Ingredients* data);
 
 	void print();
 	void print_all();
 
 private:
 	void clear();
-	// Хэш функция (умножения)
-	size_t hash_1(string key);
+	size_t GCD(size_t var_1, size_t var_2);
+
 	// Хэш функция (деление по модулю)
-	size_t hash_2(string key);
+	size_t hash_1(string key);
+	// Хэш функция (+ step)
+	size_t hash_2(size_t key);
 
 	void rehesh_less();
 	void rehesh_more();
@@ -67,8 +79,8 @@ Dynamic_Hash_Table::Dynamic_Hash_Table(size_t size, double_t less_dynamic_factor
 	this->more_dynamic_factor = more_dynamic_factor;
 
 	min_size = size;
-	step = size / ((2 / less_dynamic_factor) / more_dynamic_factor);
-	if ((step % 2 == 0))
+	step = 2;
+	while (GCD(step, size) != 1)
 		step += 1;
 
 	Hash_Table = new Dynamic_Hash_Table_Cell[size];
@@ -82,57 +94,51 @@ Dynamic_Hash_Table::~Dynamic_Hash_Table()
 
 inline void Dynamic_Hash_Table::add(Ingredients* data)
 {
-	if (seach(data->name))
-		return;
-
-	size_t pos = hash_1(data->name);
-	if (Hash_Table[pos].flag != 1)
-	{
-		Hash_Table[pos].data = data;
-		Hash_Table[pos].flag = 1;
-		size_k += 1;
-	}
+	size_t hash = hash_1(data->name);
+	if (Hash_Table[hash].flag != 1)
+		Hash_Table[hash].data = data;
 	else
 	{
-		pos = hash_2(data->name);
-		while (Hash_Table[pos].flag == 1)
+		while (Hash_Table[hash].flag == 1)
 		{
-			pos += step;
-			if (pos >= size)
-				pos -= size;
+			if (Hash_Table[hash].data->name == data->name)
+				return;
+
+			hash = hash_2(hash);
 		}
-		Hash_Table[pos].data = data;
-		Hash_Table[pos].flag = 1;
-		size_k += 1;
+
+		Hash_Table[hash].data = data;;
 	}
+
+	size_t hash_t = hash_2(hash);
+	while (hash != hash_t && Hash_Table[hash_t].flag != 0)
+		if (Hash_Table[hash_t].flag == 1 && Hash_Table[hash_t].data->name == data->name)
+			return;
+		else
+			hash_t = hash_2(hash_t);
+	Hash_Table[hash].flag = 1;
+	size_k += 1;
 
 	while (((double)size_k / (double)size) >= more_dynamic_factor || size_k == size)
 		rehesh_more();
 }
 
-inline void Dynamic_Hash_Table::delete_key(string key)
+inline void Dynamic_Hash_Table::delete_key(Ingredients* data)
 {
-	if (!seach(key))
-		return;
-
-	size_t pos = hash_1(key);
-	if (Hash_Table[pos].data->name == key && Hash_Table[pos].flag == 1)
+	size_t hash = hash_1(data->name);
+	if (Hash_Table[hash].flag == 1 && *Hash_Table[hash].data == *data)
 	{
-		Hash_Table[pos].flag = 2;
+		Hash_Table[hash].flag = 2;
 		size_k -= 1;
 	}
 	else
 	{
-		pos = hash_2(key);
-		while (Hash_Table[pos].flag == 1 && Hash_Table[pos].data->name != key || Hash_Table[pos].flag == 2)
+		while (Hash_Table[hash].flag == 1 && *Hash_Table[hash].data != *data || Hash_Table[hash].flag == 2)
+			hash = hash_2(hash);
+
+		if (Hash_Table[hash].flag == 1 && *Hash_Table[hash].data == *data)
 		{
-			pos += step;
-			if (pos >= size)
-				pos -= size;
-		}
-		if (Hash_Table[pos].data->name == key)
-		{
-			Hash_Table[pos].flag = 2;
+			Hash_Table[hash].flag = 2;
 			size_k -= 1;
 		}
 	}
@@ -141,26 +147,20 @@ inline void Dynamic_Hash_Table::delete_key(string key)
 		rehesh_less();
 }
 
-inline bool Dynamic_Hash_Table::seach(string key)
+inline bool Dynamic_Hash_Table::seach(Ingredients* data)
 {
-	size_t pos = hash_1(key);
-	if (Hash_Table[pos].flag == 1 && Hash_Table[pos].data->name == key)
+	size_t hash = hash_1(data->name);
+	if (Hash_Table[hash].flag == 1 && *Hash_Table[hash].data == *data)
 	{
 		return true;
 	}
 	else
 	{
-		pos = hash_2(key);
-		while (Hash_Table[pos].flag == 1 && Hash_Table[pos].data->name != key || Hash_Table[pos].flag == 2)
-		{
-			pos += step;
-			if (pos >= size)
-				pos -= size;
-		}
-		if (Hash_Table[pos].flag == 1 && Hash_Table[pos].data->name == key)
-		{
+		while (Hash_Table[hash].flag == 1 && *Hash_Table[hash].data != *data || Hash_Table[hash].flag == 2)
+			hash = hash_2(hash);
+
+		if (Hash_Table[hash].flag == 1 && *Hash_Table[hash].data == *data)
 			return true;
-		}
 	}
 	return false;
 }
@@ -170,17 +170,16 @@ inline void Dynamic_Hash_Table::print()
 	for (size_t i = 0; i < size; i++)
 		if (Hash_Table[i].flag == 1)
 			cout << Hash_Table[i].data->name << " " << Hash_Table[i].data->units << " " << Hash_Table[i].data->calories << endl;
-	cout << step << endl;
 }
 
 inline void Dynamic_Hash_Table::print_all()
 {
 	for (size_t i = 0; i < size; i++)
-		if (Hash_Table[i].flag == 1)
-			cout << Hash_Table[i].flag << "   " << Hash_Table[i].data->name << " " << Hash_Table[i].data->units << " " << Hash_Table[i].data->calories << endl;
+		if (Hash_Table[i].flag != 0)
+			cout << i << "        " << Hash_Table[i].data->name << " " << Hash_Table[i].data->units << " " << Hash_Table[i].data->calories << "        " << Hash_Table[i].flag << endl;
 		else
-			cout << Hash_Table[i].flag << endl;
-	cout << step << endl;
+			cout << i << "        - - -       " << Hash_Table[i].flag << endl;
+	cout << "Step = " << step << endl;
 }
 
 inline void Dynamic_Hash_Table::clear()
@@ -189,21 +188,28 @@ inline void Dynamic_Hash_Table::clear()
 		delete Hash_Table[i].data;
 }
 
-inline size_t Dynamic_Hash_Table::hash_1(string key)
+inline size_t Dynamic_Hash_Table::GCD(size_t var_1, size_t var_2)
 {
-	size_t temp = 0;
-	for (size_t i = 0; i < key.size(); i++)
-		temp += (int)key[i];
-	double_t temp_d = size * temp * less_dynamic_factor * more_dynamic_factor;
-	return (size_t)temp_d % size;
+	while (var_1 % var_2 != 0)
+	{
+		size_t temp = var_1 % var_2;
+		var_1 = var_2;
+		var_2 = temp;
+	}
+	return var_2;
 }
 
-inline size_t Dynamic_Hash_Table::hash_2(string key)
+inline size_t Dynamic_Hash_Table::hash_1(string key)
 {
-	size_t temp = 0;
+	int temp = 0;
 	for (size_t i = 0; i < key.size(); i++)
 		temp += (int)key[i];
 	return temp % size;
+}
+
+inline size_t Dynamic_Hash_Table::hash_2(size_t key)
+{
+	return (key + step) % size;
 }
 
 inline void Dynamic_Hash_Table::rehesh_less()
@@ -216,8 +222,8 @@ inline void Dynamic_Hash_Table::rehesh_less()
 		size = min_size;
 	size_k = 0;
 
-	step = size / ((2 / less_dynamic_factor) / more_dynamic_factor);
-	if ((step % 2 == 0))
+	step = 2;
+	while (GCD(step, size) != 1)
 		step += 1;
 
 	Dynamic_Hash_Table_Cell* temp = Hash_Table;
@@ -228,7 +234,6 @@ inline void Dynamic_Hash_Table::rehesh_less()
 			add(temp[i].data);
 
 	delete[] temp;
-
 }
 
 inline void Dynamic_Hash_Table::rehesh_more()
@@ -239,8 +244,8 @@ inline void Dynamic_Hash_Table::rehesh_more()
 		size = temp_size + 1;
 	size_k = 0;
 
-	step = size / ((2 / less_dynamic_factor) / more_dynamic_factor);
-	if ((step % 2 == 0))
+	step = 2;
+	while (GCD(step, size) != 1)
 		step += 1;
 
 	Dynamic_Hash_Table_Cell* temp = Hash_Table;
@@ -251,5 +256,4 @@ inline void Dynamic_Hash_Table::rehesh_more()
 			add(temp[i].data);
 
 	delete [] temp;
-
 }
